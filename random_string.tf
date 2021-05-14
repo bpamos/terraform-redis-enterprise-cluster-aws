7544bfc35f77346f80dcb15c662a6c8ca495d742
brandonamos <bpamos21@gmail.com>
2021-05-14 13:32:56 -0600

    create cluster, join cluster, run memtier, to s3

--- random_string.tf
+++ random_string.tf
@@ -1,8 +1,8 @@
 
-
-# resource "random_string" "client_vm_name" {
-#   length = 4
-#   number = true
-#   special = false
-#   upper = false
-# }
+# random string for the bucket name ending (s3 buckets cant have the same name)
+resource "random_string" "s3_bucket_name" {
+  length = 8
+  number = true
+  special = false
+  upper = false
+}
